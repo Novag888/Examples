4e361e5ed6c70f6d46be718746f5291a52ba0427
Novag888 <novag888@gmail.com>
2023-01-11 01:53:40 +0300

    004

--- OneDrive/Рабочий стол/New map/Examples/Examples004/Program.cs
+++ OneDrive/Рабочий стол/New map/Examples/Examples004/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double namderA = 12;
+double namderB = 5;
+Console.WriteLine(namderA / namderB);
